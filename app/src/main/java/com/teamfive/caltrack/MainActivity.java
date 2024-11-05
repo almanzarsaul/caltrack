a2f6b8addfd28bd21f033fd8ba0e0fdcb9735b4a
Saul Almanzar <almanzarsaul@icloud.com>
2024-11-05 07:53:41 -0500

    Fixed some code stuff

--- app/src/main/java/com/teamfive/caltrack/MainActivity.java
+++ app/src/main/java/com/teamfive/caltrack/MainActivity.java
@@ -1,117 +1,37 @@
 package com.teamfive.caltrack;
 
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.EditText;
-import android.widget.Toast;
-
-//import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.navigation.NavController;
-//import androidx.navigation.Navigation;
-//import androidx.navigation.ui.AppBarConfiguration;
-//import androidx.navigation.ui.NavigationUI;
-
-import android.app.AlertDialog;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.EditText;
-import android.widget.Toast;
-import java.util.HashMap;
-
-//import com.teamfive.caltrack.databinding.ActivityMainBinding;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.cardview.widget.CardView;
 
-import com.teamfive.caltrack.database.AppDatabase;
-import com.teamfive.caltrack.database.entities.Goals;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.teamfive.caltrack.databinding.ActivityMainBinding;
 
-import java.util.HashMap;
-
 public class MainActivity extends AppCompatActivity {
-      //private ActivityMainBinding binding;
-
-      private String selectedDate;
-      private HashMap<String, String> notesMap
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        CalendarView calendarView = findViewById(R.id.calendarView);
-        Button editNoteButton = findViewById(R.id.editNoteButton);
-        notesMap = new HashMap<>();
-
-        selectedDate = String.valueOf(calendarView.getDate());
-        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth);
-        
-        // Button click to edit notes
-        editNoteButton.setOnClickListener(v -> showNoteDialog());
-
-        //binding = ActivityMainBinding.inflate(getLayoutInflater());
-        //setContentView(binding.getRoot());
-
-        //BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-        //        R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-        //        .build();
-        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        //NavigationUI.setupWithNavController(binding.navView, navController);
-
-
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        BottomNavigationView navView = binding.navView;
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_log)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
     }
 
-    private void showNoteDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("Edit Note for " + selectedDate);
-
-
-        final EditText input = new EditText(MainActivity.this);
-        input.setText(notesMap.get(selectedDate)); // Load existing note if any
-        builder.setView(input);
-
-
-        builder.setPositiveButton("Save", (dialog, which) -> {
-            String note = input.getText().toString();
-            notesMap.put(selectedDate, note); // Save the note
-            Toast.makeText(MainActivity.this, "Note saved!", Toast.LENGTH_SHORT).show();
-        });
-        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
-
-        builder.show();
-
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment);
-
-        if (navHostFragment != null) {
-            NavController navController = navHostFragment.getNavController();
-
-            AppBarConfiguration appBarConfig = new AppBarConfiguration.Builder(
-                    R.id.navigation_home, R.id.navigation_log, R.id.navigation_notifications)
-                    .build();
-
-            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
-            NavigationUI.setupWithNavController(navView, navController);
-        }
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return navController.navigateUp() || super.onSupportNavigateUp();
     }
-
 }
-
-
-
--- app/src/main/java/com/teamfive/caltrack/ui/home/HomeFragment.java
+++ app/src/main/java/com/teamfive/caltrack/ui/home/HomeFragment.java
@@ -1,120 +1,20 @@
 package com.teamfive.caltrack.ui.home;
 
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.teamfive.caltrack.Food;
-import com.teamfive.caltrack.FoodRepository;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.teamfive.caltrack.R;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    private String selectedDate;
-    private HashMap<String, String> notesMap;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_hom3, container, false);
-        LinearLayout layout = (LinearLayout) rootView;
-
-        CalendarView calendarView = rootView.findViewById(R.id.calendarView);
-        Button editNoteButton = rootView.findViewById(R.id.editNoteButton);
-        notesMap = new HashMap<>();
-
-        selectedDate = String.valueOf(calendarView.getDate());
-        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> selectedDate = year + "-" + (month + 1) + "-" + dayOfMonth);
-
-        // Button click to edit notes
-        editNoteButton.setOnClickListener(v -> showNoteDialog());
-
-        // Create the buttons using the food names and ids from FoodRepository
-        List<Food> foodList = FoodRepository.getInstance(requireContext()).getFoods();
-        for (Food food : foodList) {
-            Button button = new Button(getContext());
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT);
-            layoutParams.setMargins(0, 0, 0, 10);   // 10px bottom margin
-            button.setLayoutParams(layoutParams);
-
-            // Display food's name on button
-            button.setText(food.getName());
-            button.setTag(food.getId());
-            // Add button to the LinearLayout
-            layout.addView(button);
-
-        /*View.OnClickListener onClickListener = itemView -> {
-            int selectedFoodId = (int) itemView.getTag();
-            Bundle args = new Bundle();
-            args.putInt( ,selectedFoodId);
-        };*/
-            //set up the recyclerView
-            RecyclerView recyclerView = rootView.findViewById(R.id.food_list);
-            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
-            FoodAdapter foodAdapter = new FoodAdapter();
-            recyclerView.setAdapter(foodAdapter);
-
-            foodAdapter.updateFoods(foodList);
-
-            return rootView;
-        }
-        return rootView;
-    }
-
-    private void showNoteDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
-        builder.setTitle("Edit Note for " + selectedDate);
-
-
-        final EditText input = new EditText(requireContext());
-        input.setText(notesMap.get(selectedDate)); // Load existing note if any
-        builder.setView(input);
-
-
-        builder.setPositiveButton("Save", (dialog, which) -> {
-            String note = input.getText().toString();
-            notesMap.put(selectedDate, note); // Save the note
-            Toast.makeText(requireActivity(), "Note saved!", Toast.LENGTH_SHORT).show();
-        });
-        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
-
-        builder.show();
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
